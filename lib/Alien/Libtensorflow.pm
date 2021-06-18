6d09bf302c813ad43800e679015f341e3182e8cc
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-06-17 21:32:41 -0400

    Add alienfile for libtensorflow nightly builds
    This downloads a nightly build of the libtensorflow library. At this
    time, this only supports systems:
    
     - Linux, 64-bit, x86
     - macOS, Version 10.12.6 (Sierra) or higher
     - Windows, 64-bit x86
    
    and the alienfile only downloads the CPU version of the build.

--- lib/Alien/Libtensorflow.pm
+++ lib/Alien/Libtensorflow.pm
@@ -1,6 +1,12 @@
 package Alien::Libtensorflow;
+# ABSTRACT: Alien for Libtensorflow machine learning library C API
 
 use strict;
 use warnings;
+
+use parent qw(Alien::Base);
+use Role::Tiny::With qw( with );
+
+with 'Alien::Role::Dino';
 
 1;
