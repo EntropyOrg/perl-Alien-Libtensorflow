924603639aa841077c569555a159da391dbde750
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-11-24 18:40:00 -0500

    Update docs and author/copyright

--- lib/Alien/Libtensorflow.pm
+++ lib/Alien/Libtensorflow.pm
@@ -1,5 +1,9 @@
 package Alien::Libtensorflow;
 # ABSTRACT: Alien for Libtensorflow machine learning library C API
+# AUTHOR: Zakariyya Mughal <zmughal@cpan.org>
+# OWNER: 2021 Zakariyya Mughal
+# OWNER: 2022 Auto-Parallel Technologies, Inc.
+# LICENSE: Apache_2_0
 
 use strict;
 use warnings;
@@ -7,3 +11,10 @@ use warnings;
 use parent qw(Alien::Base);
 
 1;
+__END__
+
+=head1 SEE ALSO
+
+L<Alien::Base>
+
+=cut
